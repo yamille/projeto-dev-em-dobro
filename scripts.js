006ad61fea0330efae3160736a7a664460d0631c
Mille <41070981+yamille@users.noreply.github.com>
2024-09-23 21:30:37 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,49 @@
+
+const alunos = [ 
+    {   name: 'Rafaela',
+        age: 26,
+        nota: 6,
+        adress: {
+            rua: "Quadra 7 conjunto D",
+            number: 36,
+            city: "Sobradinho",
+            state: "DF",
+            country: "Brasil"}
+    },
+    {   name: 'Carlos',
+        age: 34,
+        nota: 7.5,
+        adress: {
+            rua: "SHIN QI 13 conjunto 1",
+            number: 12,
+            city: "Lago Norte",
+            state: "DF",
+            country: "Brasil"}
+    },
+    {   name: 'Adriele',
+        age: 39,
+        nota: 3.5,
+        adress: {
+            rua: "Quadra 7 conjunto D",
+            number: 36,
+            city: "Sobradinho",
+            state: "DF",
+            country: "Brasil"}
+    },
+    {   name: 'Davi',
+        age: 15,
+        nota: 5,
+        adress: {
+            rua: "Quadra 5 conjunto 5D",
+            number: 19,
+            city: "Planaltina",
+            state: "DF",
+            country: "Brasil"}
+    }
+]
+
+    console.log("Olá, estamos em javascript!")
+
+const input = document.getElementById("main-input")
+
+    console.log(input)  
